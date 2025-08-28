c113d9e63937e15beeb03f86ee762eb44433cfbd
LuciaSanchezMiranda <lucia.sanchez@vallegrande.edu.pe>
2025-08-28 05:25:30 -0500

    Se añadio el encabezado y footer con estilos tailwindcss

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template, request, jsonify
+
+#Inializacion de mi flamework flask
+app = Flask(__name__)
+
+#rutas
+@app.route('/')
+
+
+def index():
+    return render_template('index.html')
+
+
+#Ejecutar mi servidor
+if __name__=='__main__':
+    app.run(debug=True)
